c13942bfbc62c9ffb3b524eeaeb2ebbfe85777c5
FabioHPascoal <fabio.pascoal@edu.ufes.br>
2022-12-10 15:59:34 -0300

    Jogo completamente refeito

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#Arquivo principal, a execução começa por aqui
 from jogo import JogoPOO
 
 def main(): 
-    jogo = JogoPOO() #Cria objeto do tipo JogoPOO
-    jogo.rodar() #Executa o método rodar() do objeto jogo
+    jogo = JogoPOO()
+    jogo.rodar()
 
 if __name__ == "__main__":
     main()
