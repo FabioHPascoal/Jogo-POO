59d48729a473b49188c6a93f35e3d954ea85201c
ryanpascoal <ryansantospascoal2@gmail.com>
2022-12-11 05:22:16 -0300

    Animação no painel de seleção

--- cena_selecao_personagem.py
+++ cena_selecao_personagem.py
@@ -5,22 +5,55 @@ from configs import*
 class cenaSelecaoPersonagem:
     def __init__(self,tela):
         self.frameRate = pg.time.Clock()
-        self.tela = tela
         #CAPTURAR SUPERFÍCIE DA TELA
+        self.tela = tela
         self.superficie_tela = pg.display.get_surface()
         self.fonte = pg.font.Font(None,40)
         self.retanguloJ1 = pg.rect.Rect(30,30,200,200)
         self.retanguloJ2 = pg.rect.Rect(Configs.LARGURA_TELA/2+30,30,200,200)
         self.rodando = True
         self.listaPersonagens = ['arqueiro','ladino','cavaleiro']
-        self.selecionadoJ1 = 'classe'
-        self.selecionadoJ2 = 'classe'
+        self.selecionadoJ1 = 'arqueiro'
+        self.selecionadoJ2 = 'arqueiro'
         self.i1 = 0
         self.i2 = 0
         self.sprites = []
         self.tempoMarcado = 0
         self.selecaoConcluida = [False,False]
         self.tempo = 0
+        self.sprite_sheet_arqueiro = pg.image.load("sprites/arqueiro.png").convert_alpha()
+        self.sprites_arqueiro = []
+        self.sprite_sheet_cavaleiro = pg.image.load("sprites/cavaleiro.png").convert_alpha()
+        self.sprites_cavaleiro = []
+        self.sprite_sheet_ladino = pg.image.load("sprites/ladino.png").convert_alpha()
+        self.sprites_ladino = []
+        self.frame_atual = 3
+        self.tempo_anterior = 0
+
+        # sprites arqueiro
+        contadorFrames = 0
+        for _ in range(9):
+            self.sprites_arqueiro.append(self.sprite_selecionado(self.sprite_sheet_arqueiro, contadorFrames, Configs.dimensoes_sprite['arqueiro']))
+            contadorFrames += 1
+
+        # sprites cavaleiro
+        contadorFrames = 0
+        for _ in range(9):
+            self.sprites_cavaleiro.append(self.sprite_selecionado(self.sprite_sheet_cavaleiro, contadorFrames,Configs.dimensoes_sprite['cavaleiro']))
+            contadorFrames += 1
+
+        # sprites ladino
+        contadorFrames = 0
+        for _ in range(9):
+            self.sprites_ladino.append(self.sprite_selecionado(self.sprite_sheet_ladino, contadorFrames,Configs.dimensoes_sprite['ladino']))
+            contadorFrames += 1
+
+    def sprite_selecionado(self, sheet, frame,dimensoes):
+        imagem = pg.Surface(dimensoes).convert_alpha()
+        imagem.blit(sheet, (0, 0), (frame * dimensoes[0], 0, dimensoes[0], dimensoes[1]))
+        imagem = pg.transform.scale(imagem, (dimensoes[0] * 3, dimensoes[1] * 3))
+        imagem.set_colorkey((0, 0, 0, 0))
+        return imagem
 
     def desenharPainel(self):
         self.tela.fill(Configs.BRANCO)
@@ -39,6 +72,31 @@ class cenaSelecaoPersonagem:
         retangulo2 = superficie_dadosJ2.get_rect(topleft = (1000,30))
         superficie_tela.blit(superficie_dadosJ1,retangulo1)
         superficie_tela.blit(superficie_dadosJ2,retangulo2)
+
+        self.dicionario = {'arqueiro': self.sprites_arqueiro[self.frame_atual],'cavaleiro':self.sprites_cavaleiro[self.frame_atual],
+        'ladino':self.sprites_ladino[self.frame_atual]}
+
+        self.posicao_rect = [self.retanguloJ1.centerx- Configs.subracao_rect[self.selecionadoJ1][0] * 3, 
+                             self.retanguloJ1.centery - Configs.subracao_rect[self.selecionadoJ1][1] * 3 + 40]
+
+        self.superficie_tela.blit(self.dicionario[self.selecionadoJ1], self.posicao_rect)
+        if pg.time.get_ticks() - self.tempo_anterior >= Configs.DURACAO_FRAME:
+            self.frame_atual += 1
+
+            if self.frame_atual == len(self.sprites_arqueiro):
+                self.frame_atual = 0
+            self.tempo_anterior = pg.time.get_ticks()
+
+        self.posicao_rect = [self.retanguloJ2.centerx- Configs.subracao_rect[self.selecionadoJ2][0] * 3, 
+                             self.retanguloJ2.centery - Configs.subracao_rect[self.selecionadoJ2][1] * 3 + 40]
+
+        self.superficie_tela.blit(self.dicionario[self.selecionadoJ2], self.posicao_rect)
+        if pg.time.get_ticks() - self.tempo_anterior >= Configs.DURACAO_FRAME:
+            self.frame_atual += 1
+
+            if self.frame_atual == len(self.sprites_arqueiro):
+                self.frame_atual = 0
+            self.tempo_anterior = pg.time.get_ticks()
 
     def escolherPersonagem(self):
         pg.event.get()
