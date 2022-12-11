4f0efbc1e7c7fe94ad8eb415cc053f2949a7214b
FabioHPascoal <fabio.pascoal@edu.ufes.br>
2022-12-11 20:40:34 -0300

    Desgraça

--- habilidades.py
+++ habilidades.py
@@ -2,9 +2,30 @@ import pygame as pg
 from funcoes import Funcoes
 from configs import Configs
 
+class Espadada(pg.sprite.Sprite):
+    def __init__(self, posicao, direcao, image):
+        super().__init__()
+        self.projetil = False
+        
+        self.direcao = direcao
+        self.posicao = posicao
+        self.velocidade = 0
+
+        self.image = image
+        self.rect = self.image.get_rect(center = self.posicao)
+        self.mask = pg.mask.from_surface(self.image)
+
+        self.direcaoX = Funcoes.sinal(self, self.direcao[0])
+        self.direcaoY = Funcoes.sinal(self, self.direcao[1])
+ 
+    def update(self):
+        self.rect.x += self.velocidade * self.direcaoX
+        self.rect.y += self.velocidade * self.direcaoY
+
 class Flecha(pg.sprite.Sprite):
     def __init__(self, posicao, direcao, image):
         super().__init__()
+        self.projetil = True
  
         self.direcao = direcao
         self.posicao = posicao
@@ -21,10 +42,31 @@ class Flecha(pg.sprite.Sprite):
         self.rect.x += self.velocidade * self.direcaoX
         self.rect.y += self.velocidade * self.direcaoY
 
-class Espadada(pg.sprite.Sprite):
+class Fireball(pg.sprite.Sprite):
     def __init__(self, posicao, direcao, image):
         super().__init__()
+        self.projetil = True
+        
+        self.direcao = direcao
+        self.posicao = posicao
+        self.velocidade = Configs.velocidade_projeteis["fireball"]
+
+        self.image = image
+        self.rect = self.image.get_rect(center = self.posicao)
+        self.mask = pg.mask.from_surface(self.image)
+
+        self.direcaoX = Funcoes.sinal(self, self.direcao[0])
+        self.direcaoY = Funcoes.sinal(self, self.direcao[1])
  
+    def update(self):
+        self.rect.x += self.velocidade * self.direcaoX
+        self.rect.y += self.velocidade * self.direcaoY
+
+class Fire_floor(pg.sprite.Sprite):
+    def __init__(self, posicao, direcao, image):
+        super().__init__()
+        self.projetil = False
+        
         self.direcao = direcao
         self.posicao = posicao
         self.velocidade = 0
